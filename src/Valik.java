ec01e7e2027fdc8b602f92e8666d5e68d10cd73e
Priit Rätsep <pratsep@itcollege.ee>
2016-09-28 20:50:24 +0300

    Ring

---
+++ src/Valik.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class Valik extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane stack = new StackPane();
+        Scene scene = new Scene(stack, 150, 150);
+        primaryStage.setScene(scene);
+
+        Circle ring = new Circle(30);
+        stack.getChildren().add(ring);
+
+        primaryStage.show();
+
+    }
+}
